c3421884d2ddc81b03b110087d378372cdf6ea5b
ardelsaut <ar.delsaut@outlook.com>
2023-11-24 17:26:16 +0100

    Create 03.Backup_All.ps1

---
+++ 03.Backup/03.Backup_All.ps1
@@ -0,0 +1,5 @@
+﻿$ALLPs1 = (Get-ChildItem "$env:TEMP\NonoOS\Menus\03.Backup\*" -Exclude "03.Backup_All.ps1").FullName
+
+foreach ($Item in $ALLPs1) {
+    Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File $Item -WindowStyle Normal" -NoNewWindow -Wait
+}
