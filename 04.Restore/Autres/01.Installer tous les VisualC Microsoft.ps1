9bc8d71bb6b007a2be78789725e4993cfefbe153
ardelsaut <ar.delsaut@outlook.com>
2023-11-24 05:59:15 +0100

    .

--- 04.Restore/Autres/01.Installer tous les VisualC Microsoft.ps1
+++ 04.Restore/Autres/01.Installer tous les VisualC Microsoft.ps1
@@ -1,2 +1,3 @@
 ﻿Expand-Archive -Path "C:\Users\Nono\AppData\Local\Temp\NonoOS\Menus\04.Restore\Autres\4.VisualC.zip" -DestinationPath "C:\Users\Nono\AppData\Local\Temp\NonoOS\Menus\04.Restore\Autres"
+
 cmd /c "C:\Users\Nono\AppData\Local\Temp\NonoOS\Menus\04.Restore\Autres\4.VisualC\install_all.bat"
