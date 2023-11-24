c75ca408a1cdc3878a8b558776f63c9e48ad7164
ardelsaut <ar.delsaut@outlook.com>
2023-11-24 17:28:02 +0100

    Update 03.Backup_All.ps1

--- 03.Backup/03.Backup_All.ps1
+++ 03.Backup/03.Backup_All.ps1
@@ -1,4 +1,4 @@
-﻿$ALLPs1 = (Get-ChildItem "$env:TEMP\NonoOS\Menus\03.Backup\*" -Exclude "03.Backup_All.ps1").FullName
+﻿$ALLPs1 = (Get-ChildItem "$env:TEMP\NonoOS\Menus\03.Backup\*" -Exclude "*.Backup_All.ps1").FullName
 
 foreach ($Item in $ALLPs1) {
     Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File $Item -WindowStyle Normal" -NoNewWindow -Wait
