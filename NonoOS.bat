b50deace3f1593f9dc9114d8ef68917e0f6bd375
ardelsaut <ar.delsaut@outlook.com>
2023-11-24 05:57:48 +0100

    .

--- NonoOS.bat
+++ NonoOS.bat
@@ -12,6 +12,7 @@
     if not exist "%dirPath%\Menus" (
     mkdir "%dirPath%\Menus"
     )
+    echo "Telechargement de l' archive... un peu de patience."
     powershell /NoLogo /NoProfile /Command "(New-Object Net.WebClient).DownloadFile('https://github.com/ardelsaut/75c9b4a9-0012-453d-9e44-e84a458c035c/zipball/master/', '%dirPath%\base.zip')"
     powershell /nologo /noprofile /command ^
         "&{[ScriptBlock]::Create((cat """%~f0""") -join [Char[]]10).Invoke(@(&{$args}%*))}"
@@ -21,9 +22,11 @@
 $inputString = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 $username = Split-Path -Path $inputString -Leaf
 Expand-Archive -Path "C:\Users\$username\AppData\Local\Temp\NonoOS\base.zip" -DestinationPath "C:\Users\$username\AppData\Local\Temp\NonoOS\test"
-Remove-Item -Path "C:\Users\$username\AppData\Local\Temp\NonoOS\base.zip" -Force
 
 Copy-Item -Path "C:\Users\$username\AppData\Local\Temp\NonoOS\test\ardelsaut-75c9b4a9-0012-453d-9e44-e84a458c035c-*\*" -Destination "C:\Users\$username\AppData\Local\Temp\NonoOS\Menus" -Force -Recurse
+
+Remove-Item -Path "C:\Users\$username\AppData\Local\Temp\NonoOS\base.zip" -Force
+Remove-Item -Path "C:\Users\$username\AppData\Local\Temp\NonoOS\test" -Force -Recurse
 
 Clear-Host
 Write-Host "Veuillez choisir le(s) script(s) a executer..." -ForegroundColor Cyan
