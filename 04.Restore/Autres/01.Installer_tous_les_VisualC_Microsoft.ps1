d20444fd0e9f20f09e918d447d0a2da6308750a2
ardelsaut <ar.delsaut@outlook.com>
2023-11-24 06:03:27 +0100

    .

---
+++ 04.Restore/Autres/01.Installer_tous_les_VisualC_Microsoft.ps1
@@ -0,0 +1,4 @@
+﻿$FichierArchive = "$env:TEMP\NonoOS\Menus\04.Restore\Autres"
+Expand-Archive -Path "$FichierArchive\4.VisualC.zip" -DestinationPath "$FichierArchive"
+
+cmd /c "$FichierArchive\4.VisualC\install_all.bat"
